6b23a59ce60335d4ce03ca82a69f8a436cc6e684
naphees <naphees9997@gmail.com>
2026-08-05 09:37:34 +0530

    Day 2 : Completed Next.js routing and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
+"use client";
 import Image from "next/image";
-
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
         <div className="mt-20px">
              <h1>Home Page </h1>
+             <h2>Next.js journey</h2>
+             <h3>Day 1: Intro next.js and file based routing</h3>
+             <h3>Day 2: All routing methods</h3>
+             <Link href={"/about"}>Go To about by Link </Link>
+             <br/><br/>
+             <button onClick={()=> router.push("/contact")}>
+              Go To Contact by {"router.push();"}method
+             </button>
+              <br/><br/>
+             <button onClick={()=> router.replace("/about")}>
+              Go To About by {"router.replace();"} method
+             </button>
+             <br/><br/>
+             <button onClick={()=> router.forward()}>
+               {"router.forward();"} method used for move forward method
+             </button>
+             <br/><br/>
+
+             <button onClick={()=> router.back()}>
+                {"router.back();"} method used for move back method
+             </button>
+             <br/><br/>
+             <button onClick={()=> router.refresh()}>
+              Refresh page by  {"router.refresh();"} method
+             </button>
+             <br/><br/>
+             <button onClick={()=> router.prefetch("/about")}>
+                 load a page in advanace by {"router.prefetch()"} method
+             </button>
 
         </div>
   );
